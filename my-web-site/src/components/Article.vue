407d8d29a9f6f5ab294eea1a8a475ca588f12c9f
Morgane <morgane.cabrol@conformit.com>
2021-01-28 15:03:10 -0500

    style

--- my-web-site/src/components/Article.vue
+++ my-web-site/src/components/Article.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="article">
-Mon menu
+    <span class="article__title">{{title}}</span>
+    <p class="article__resume">{{resume}}</p>
   </div>
 </template>
 
@@ -8,15 +9,34 @@ Mon menu
 export default {
   name: 'Article',
   props: {
-
+    title:{
+      type: String,
+      required: true
+    },
+    resume:{
+      type: String,
+      default: ''
+    }
   }
 }
 </script>
 
 <style lang="scss">
+@import '~@/assets/sass/variables.scss';
 .article {
   width: 100%;
-  border: solid 1px black;
-  height: 30em;
+  height: auto;
+  padding: 5em;
+  border-bottom: solid 1px $color-dark-blue;
+  &__title {
+    color: $color-dark-blue;
+    font-size: 1.5em;
+  }
+
+  &__resume {
+    color: $color-dark-blue;
+    font-size: 1em;
+    height: 100%;
+  }
 }
 </style>
